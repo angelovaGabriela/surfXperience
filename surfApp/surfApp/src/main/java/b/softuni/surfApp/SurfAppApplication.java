3e2de09773c2200b4642374baa5a54e02630763b
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-12-04 00:20:37 +0100

    Last added: surfApp - project set up - index page

---
+++ surfApp/surfApp/src/main/java/b/softuni/surfApp/SurfAppApplication.java
@@ -0,0 +1,13 @@
+package b.softuni.surfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfAppApplication.class, args);
+	}
+
+}
---
+++ surfApp/surfApp/src/test/java/b/softuni/surfApp/SurfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package b.softuni.surfApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
