9ade77cc31adf24b8aec60864ea9ea716ad6d7a2
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2023-12-11 15:08:27 +0100

    Last added: equipment types initialized

--- surfApp/surfApp/src/main/java/b/softuni/surfApp/init/DatabaseInit.java
+++ surfApp/surfApp/src/main/java/b/softuni/surfApp/init/DatabaseInit.java
@@ -29,7 +29,7 @@ public class DatabaseInit implements CommandLineRunner {
     public void run(String... args) throws Exception {
         userService.initProfiles();
         commentService.initCommentMoods();
-//        equipmentService.initEquipmentTypes();
+        equipmentService.initEquipmentTypes();
 //        campService.initCampLevels();
     }
 }
--- surfApp/surfApp/src/main/java/b/softuni/surfApp/service/impl/EquipmentServiceImpl.java
+++ surfApp/surfApp/src/main/java/b/softuni/surfApp/service/impl/EquipmentServiceImpl.java
@@ -1,9 +1,32 @@
 package b.softuni.surfApp.service.impl;
 
+import b.softuni.surfApp.model.entity.CommentMood;
+import b.softuni.surfApp.model.entity.EquipmentType;
+import b.softuni.surfApp.model.enums.EquipmentTypeEnum;
+import b.softuni.surfApp.repository.EquipmentTypeRepository;
 import b.softuni.surfApp.service.EquipmentService;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
 
 @Service
 public class EquipmentServiceImpl implements EquipmentService {
 
+    private final EquipmentTypeRepository typeRepository;
+
+    public EquipmentServiceImpl(EquipmentTypeRepository typeRepository) {
+        this.typeRepository = typeRepository;
+    }
+
+    @Override
+    public void initEquipmentTypes() {
+        if (this.typeRepository.count() == 0) {
+            Arrays.stream(EquipmentTypeEnum.values())
+                    .forEach(type -> {
+                        EquipmentType equipmentType = new EquipmentType();
+                        equipmentType.setName(type);
+                        this.typeRepository.save(equipmentType);
+                    });
+        }
+    }
 }
