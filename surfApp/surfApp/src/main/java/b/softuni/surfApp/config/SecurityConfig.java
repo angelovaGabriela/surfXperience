707354671b2a87a6305058bc292795845cc36a9a
Gabriela Angelova <angelova.gabriela.emilova@gmail.com>
2024-07-25 19:03:24 +0200

    Last added: spring security config started
    *spring security config class deleted because of many deprecated features

---
+++ surfApp/surfApp/src/main/java/b/softuni/surfApp/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package b.softuni.surfApp.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+      return  httpSecurity.authorizeHttpRequests(
+              //Setup which URLs are available to who
+              authorizeRequests ->
+                      authorizeRequests
+                              // All static resources in "common locations" (css, images, js) are available to anyone
+                              .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                              // Some more resources for all users
+                              .requestMatchers("/", "/login", "/register").permitAll()
+                              // All other Urls should be authenticated
+                              .anyRequest()
+                              .authenticated()
+              )
+                .formLogin(formLogin ->
+                        formLogin
+                                // Where is our custom login form ?
+                                .loginPage("/login")
+                                // What is the name of the username parameter in the Login POST request?
+                                .usernameParameter("username")
+                                // What is the name of the password parameter in the Login POST request?
+                                .passwordParameter("password")
+                                // What happens if the Login is successful ?
+                                .defaultSuccessUrl("/")
+                                //What happens if the Login fails ?
+                                .failureForwardUrl("/login-error")
+                )
+                .logout(
+                        logout ->
+                                // Which is the logout URL
+                                logout.logoutUrl("/logout")
+                                        // Where to go in case of success
+                                        .logoutSuccessUrl("/")
+                                        // invalidate the session after logout
+                                        .invalidateHttpSession(true)
+                )
+                .build();
+
+    }
+
+}
